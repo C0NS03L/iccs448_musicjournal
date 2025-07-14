b31edcfd9c3dc2c8de6dc4e1039c862bdfba5771
C0NS03L <58496201+C0NS03L@users.noreply.github.com>
2025-07-14 16:21:01 +0700

    fix(build): fix SDK version errors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
 }
 
 dependencies {
@@ -14,7 +14,7 @@ dependencies {
 android {
     namespace = "com.c0ns03l.iccs448_musicjournal"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -30,7 +30,7 @@ android {
         applicationId = "com.c0ns03l.iccs448_musicjournal"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
